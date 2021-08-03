374361495641565716319dfacf1d7757dd8bae41
seongmin <tjdalsdl0219@gmail.com>
2021-08-03 23:07:18 +0900

    article collaborative learn

---
+++ articleRecomCF/animeRecomCF.py
@@ -0,0 +1,22 @@
+
+
+import numpy as np
+import pandas as pd
+import scipy
+import math
+import random
+import sklearn
+from nltk.corpus import stopwords
+from scipy.sparse import csr_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.sparse.linalg import svds
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    articles_df = pd.read_csv('../input/shared_articles.csv')
+    articles_df = articles_df[articles_df['eventType'] == 'CONTENT SHARED']
+    articles_df.head(5)
